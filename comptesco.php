b861e467a92ef4a6f61305678d0bbf85b48e505c
anaeservant <anae.servant@hotmail.fr>
2023-05-06 14:16:26 +0200

    sco

---
+++ comptesco.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <title>OMNES MySkills</title>
+    <link rel="stylesheet" type="text/css" href="comptesco.css">
+  </head>
+
+  <body>
+    Compte du prof ...
+  </body>
+
+</html>
