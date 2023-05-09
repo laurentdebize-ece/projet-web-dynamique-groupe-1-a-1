59e7dddecfcefcbba4a640db8af9992b50804df4
evacourvaniotis <eva.courvaniotis@edu.ece.fr>
2023-05-09 17:13:12 +0200

    change

--- accueil.php
+++ accueil.php
@@ -6,16 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="accueil.css">
-    <title>Omnes Skills</title>   
+    <title>Omnes MySkills</title>   
 </head>
 
 <body>
-	<h1>Omnes Skills</h1>
+	<h1>Omnes MySkills</h1>
 	<form method="post" action="accueil2.php">
-		<label for="login">Login:</label>
+		<label for="login">Identifiant :</label>
 		<input type="email" name="login" id="login">
 
-		<label for="password">Mot de passe:</label>
+		<label for="password">Mot de passe :</label>
 		<input type="password" name="password" id="password">
 
 		<select name="statut">
@@ -25,7 +25,7 @@
 		</select>
 
 		<input type="submit" value="Connexion">
-		<input type="submit" value="Créer son compte">
+		
 	</form>
            
 </body>
