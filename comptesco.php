30ec6cd7c6dc0022c84325e350099e4a31b94f94
Trystan <trystan.dubois@edu.ece.fr>
2023-05-20 16:14:03 +0200

    front compte

--- comptesco.php
+++ comptesco.php
@@ -1,4 +1,4 @@
-<?php session_start();?>
+<?php session_start(); ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -8,22 +8,45 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" type="text/css" href="comptesco.css">
+    <link rel="stylesheet" type="text/css" href="compteScoFront.css">
 
-    <title>compte scolarité</title>
+    <title>OMNES MySkills</title>
 </head>
 
 <body>
-    <h1>Mon compte Administrateur :</h1>
-    <form>
-        <p> Nom :  <?php echo $_SESSION['Nom'] ?> </p>
-        <p>Prénom : <?php echo $_SESSION['Prenom'] ?> </p>
-        <p>Adresse mail : <?php echo $_SESSION['login'] ?></p>
-        <p>Mot de passe : <?php echo $_SESSION['password'] ?></p>
-        <br>
+    <header>
+        <img src="logoSite.png" alt="imageLogo">
+        <h1>Mon compte</h1>
+        <nav>
+            <button onclick="window.location.href='etudiant.php'">Retour</button>
+            <button id="deco">Déconnexion</button>
+        </nav>
+    </header>
+    <div class="page"> <!--contenant toute la page-->
+        <div class="pageGauche"><!--gauche page-->
+            <h2>Bienvenue!</h2>
+            <form>
+                <p> Nom : <?php echo $_SESSION['Nom'] ?> </p>
+                <p>Prénom : <?php echo $_SESSION['Prenom'] ?> </p>
+                <p>Adresse mail : <?php echo $_SESSION['login'] ?></p>
+                <p>Mot de passe : <?php echo $_SESSION['password'] ?></p>
+            </form>
+        </div>
+        <div class="pageDroite"><!--droite page-->
+            <img class="profilImage" src="ProfilD.jpg" alt="Photo de profil">
+        </div>
+    </div>
+    <!--pop-up déconnexion-->
+    <script>
+        document.getElementById("deco").addEventListener("click", decOut);
 
-    </form>
-    <button onclick="window.location.href='scolarite.php'">Retour</button>
+        function decOut() {
+            if (confirm("Êtes-vous sûr de vouloir vous déconnecter ?")) {
+                /*retour page MDP*/
+                window.location.href = "accueil.php";
+            }
+        }
+    </script>
 
     <div id="footer">
         <p>© 2023 Projet WEB Dynamique: Eva, Anaé, Valentin, Trystan</p>
